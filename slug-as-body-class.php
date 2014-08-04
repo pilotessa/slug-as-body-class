4024bdf0585aa14dc4fea4bfd4f5dc713b78a3c0
pilotessa <pilotessa@gmail.com>
2014-08-04 15:09:33 +0300

    Single pages only

--- slug-as-body-class.php
+++ slug-as-body-class.php
@@ -11,7 +11,7 @@ add_filter( 'body_class', 'slug_as_body_class' );
 function slug_as_body_class( $classes ) {
 	global $post;
 
-	if ( isset( $post ) ) {
+	if ( isset( $post ) && is_singular() ) {
 		$classes[] = $post->post_type . '-' . $post->post_name;
 	}
 
